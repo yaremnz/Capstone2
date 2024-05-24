17d3d7ac5273f73d6a533adf72ab8f100411a464
Yareli Cerda <yarelic.1002@gmail.com>
2024-05-24 01:45:22 -0400

    much needed edits and README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Capstone2
+
+##Home Page
+Using bootrap I was ablw to make cards and make the navigation bar accordingly. 
+<img alt="Home Page" width="100%" src="./images/homepage.PNG">
+<img alt="Home Page" width="100%" src="./images/homepage2.PNG">
+
+##Parks Page
+Here we have a radio and select options. Using CSS I was able to sperate and design the option results. 
+Using Javascript I was able to make a function that when an option is selected (location vs park type) it will make the other option disapear. 
+<img alt="Parks Page" width="100%" src="./images/parkspage.PNG">
+
+##Mountain Page 
+Using Float in my mountains.css allowed me to be able to place the image of the mountain next to it's information.
+Withing in my Javascript for mountains.js, I was able to make a little table to display certain information regarding the mountain selected. 
+<img alt="Mountains Page" width="100%" src="./images/mountainspage.PNG">
+
--- scripts/mountains.js
+++ scripts/mountains.js
@@ -21,20 +21,6 @@ for(m of mountainsArray){
         mountainList.appendChild(o);
 }
 
-// {
-//     "results": {"sunrise":"5:18:00 AM",
-//     "sunset":"6:43:52 PM",
-//     "solar_noon":"12:00:56PM",
-//     "day_length":"13:25:52",
-//     "civil_twilight_begin":"4:49:23 AM",
-//     "civil_twilight_end":"7:12:29 PM",
-//     "nautical_twilight_begin":"4:13:02 AM",
-//     "nautical_twilight_end":"7:48:50 PM",
-//     "astronomical_twilight_begin":"3:34:23 AM",
-//     "astronomical_twilight_end":"8:27:29 PM"},
-//     "status":"OK"
-//     }
-
 mountainList.addEventListener("change", async e=>{
     const mountain = mountainsArray[mountainList.selectedIndex - 1];
     const sunData = await getSunsetForMountain(mountain.coords.lat, mountain.coords.lng);
@@ -55,24 +41,7 @@ mountainResults.innerHTML = `
     <br><br>
 <p> ${mountain.desc} </p>
 `;
-
-    // Elevation: ${mountain.elevation} feet <br>
-
-    // Effort: ${mountain.effort}<br>
-    // Lattitude/Longitude: (${mountain.coords.lat}, ${mountain.coords.lng})
-    // sunraise: $ <br>
-    // 
-
-    // <p>${mountain.desc}</p>
-    
-    // `;;
 });
-
-// mountainsArray.forEach(m => mountainList.appendChild(option(m.name)));
-
-// // mountainResults
-
-
 
 });
 
